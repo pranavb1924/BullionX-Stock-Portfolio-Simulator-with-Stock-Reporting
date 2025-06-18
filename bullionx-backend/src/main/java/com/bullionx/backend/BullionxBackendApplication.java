148b262524e285268bdcdb2f25cd2ed63c9f3590
Pranav B <bhoopal.p@northeastern.edu>
2025-06-18 14:37:30 -0400

    SpringBoot scaffolding for back end

---
+++ bullionx-backend/src/main/java/com/bullionx/backend/BullionxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bullionx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullionxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullionxBackendApplication.class, args);
+	}
+
+}
---
+++ bullionx-backend/src/test/java/com/bullionx/backend/BullionxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bullionx.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullionxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
